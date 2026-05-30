d8d723503b258ecdfe1967ecb66369c0599e8090
Eman Firooz <emanfirooz@gmail.com>
2026-05-30 19:24:17 +0430

    create a program to find the area of  a circle

---
+++ Java-Learning-Ch01-Basics/ComputeAreaWithConstant/Main.java
@@ -0,0 +1,13 @@
+package ComputeAreaWithConstant;
+
+public class Main {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+
+        System.out.println( PI );
+        //Create a Scanner object
+        //Prompt the user to enter a radius
+        //Compute area
+        //Display result
+    }
+}
