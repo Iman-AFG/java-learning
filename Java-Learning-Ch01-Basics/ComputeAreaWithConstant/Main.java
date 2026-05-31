ae3398f2a32bcc51d97ecedc6b801a5d245b57d9
Eman Firooz <emanfirooz@gmail.com>
2026-05-31 09:07:11 +0430

    ComputeAreaWithConstant: Final touch and yeah it's working

--- Java-Learning-Ch01-Basics/ComputeAreaWithConstant/Main.java
+++ Java-Learning-Ch01-Basics/ComputeAreaWithConstant/Main.java
@@ -1,13 +1,25 @@
 package ComputeAreaWithConstant;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         final double PI = 3.14159;
 
-        System.out.println( PI );
         //Create a Scanner object
+        Scanner input = new Scanner(System.in);
+
         //Prompt the user to enter a radius
+        System.out.print("Enter a number for radius: ");
+        double radius = input.nextDouble();
+
         //Compute area
+        double area = PI * radius * radius ;
+
         //Display result
+        System.out.println("The area for the circle of radius "
+                             + radius + " is " + area);
+
+        input.close();
     }
 }
